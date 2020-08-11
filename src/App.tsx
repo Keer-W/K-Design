5bafd45032e5e237a913afa71ad1992777bcc6a4
Keer-W <keer964@gmail.com>
2020-08-11 23:50:40 +0800

    小改动

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Keer</h1>
+        <h2>Hello Keer</h2>
+        <h3>Hello Keer</h3>
+        <h4>Hello Keer</h4>
+        <hr></hr>
+        <code>let a  = 3</code>
+        <a>Learn React</a>
       </header>
     </div>
   );
