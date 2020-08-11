2bf72f736467cd06f9dbde746aa40c5684fd1afd
Keer-W <keer964@gmail.com>
2020-08-11 15:11:27 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const a = '123'
-  if(a == '123') {
-    console.log('k')
-  }
   return (
     <div className="App">
       <header className="App-header">
